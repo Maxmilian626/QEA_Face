3a609006b02bf78a0e01d5989cba6def57a5880f
Oktober13 <lydiazuehsow@gmail.com>
2016-04-25 12:18:01 -0400

    Final vers. Eigenface Facial Recognition!

--- faceclass.m
+++ faceclass.m
@@ -1,85 +1,96 @@
 function res = faceclass()
 
 load classdata.mat;
-
-image1 = classdata(:,:,1);
-image2 = classdata(:,:,90);
-image3 = classdata(:,:,120);
-image4 = classdata(:,:,145);
-image5 = classdata(:,:,175);
-
-[m,n] = size(image1);
-
-image1 = reshape(image1, m*n,1);
-image2 = reshape(image2, m*n,1);
-image3 = reshape(image3, m*n,1);
-image4 = reshape(image4, m*n,1);
-image5 = reshape(image5, m*n,1);
+% 
+% image1 = classdata(:,:,1);
+% image2 = classdata(:,:,90);
+% image3 = classdata(:,:,120);
+% image4 = classdata(:,:,145);
+% image5 = classdata(:,:,175);
+% 
+% [m,n] = size(image1);
+% 
+% image1 = reshape(image1, m*n,1);
+% image2 = reshape(image2, m*n,1);
+% image3 = reshape(image3, m*n,1);
+% image4 = reshape(image4, m*n,1);
+% image5 = reshape(image5, m*n,1);
 
 %Array for picture to picture correlation
 %Concatenated horizontally into matrix A
-A = [image1 image2 image3 image4 image5];
-mean_pixelsA = (mean(A'))';
+% TestFaces = [image1 image2 image3 image4 image5];
 
-% size(A)
-% size(mean_pixelsA)
-maxrow = size(A(1,:));
+TestFaces = []
 
-for row = 1:maxrow(2)
-%     size(A(:,row))
-%     size(mean_pixelsA)
-    A(:,row) = A(:,row) - mean_pixelsA;
+for imagenum = 1:343
+    image = classdata(:,:,imagenum);
+    [m,n] = size(image);
+    image = reshape(image, m*n,1);
+    TestFaces(:,imagenum) = image;
 end
 
-At = transpose(A);
+numpics = size(TestFaces);
+numpics = numpics(:,2)
 
+mean_pixels = (mean(TestFaces'))';
+maxrow = size(TestFaces(1,:));
 
-cov = At*A;
+% Normalize test faces
+for row = 1:maxrow(2)
+    normA(:,row) = TestFaces(:,row) - mean_pixels;
+end
+
+normAt = transpose(normA);
+
+% Find covariance matrix, eigenvalues, and eigenvectors
+cov = normAt*normA;
 [vec,eigen] = eig(cov);
-% vec(:,1)
 
-newvec = A * vec;
+% Find eigenfaces
+newvec = normA * vec;
 
+% Normalizing eigenfaces?
 for row = 1:92160
-    for column = 1:5
+    for column = 1:numpics
     newvec(row, column) = norm(newvec(row, column));
     end
 end
 
-eigfaces = newvec + repmat(mean_pixelsA,1,5);
+%Re-adding mean face
+eigfaces = newvec + repmat(mean_pixels,1,numpics);
 
-image1 = reshape(eigfaces(:,1), m,n);
-% imshow(image1)
+% image1 = reshape(eigfaces(:,1), m,n);
+% imshow(image1);
 
 %%
-%Image Recognition
+% Finding weights of training sets
 
-input = classdata(:,:,90);
+% TestFaces;
+
+testweight = transpose(normA) * eigfaces;
+size(TestFaces)
+% size()
+
+%%
+% Image Recognition
+
+input = classdata(:,:,344);
+imshow(input)
 input = reshape(input, m*n,1);
 
-difference = input - mean_pixelsA;
+difference = input - mean_pixels;
 
-% for column = 1:5
-    
-% weight = transpose(newvec) * repmat(difference,1,5) * newvec
-size(newvec)
-size(difference)
+figure
 
-% M = min(newvec,[],1)
-% % size (difference)
-% % size(newface)
-% 
-% %Finding Euclidean distance
-% distance = abs(newface - repmat(difference,1,5));
-% threshhold = 1e-6;
+weight = transpose(repmat(difference, 1,numpics)) * eigfaces;
 
-% size(distance)
-% M = min(distance,[],1)
-% 
-% maxrow = size(M');
-% for column = 1:maxrow(1)
-%     if M(column) <= threshhold
-%         column
-%     end
-% end
+%Finding Euclidean distance
+
+distance = abs(weight - testweight);
+threshhold = 500;
+
+[M,I] = min(distance, [], 1);
+
+match = reshape(TestFaces(:,I(1)),m,n);
+imshow(match)
 end
